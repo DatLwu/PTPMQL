dc4bc600d79d20f61718ebf9e47688a95e4dfb43
DatLwu <datluu351@gmail.com>
2023-08-24 19:12:54 +0700

    Creat_HTML&Project

--- console_readline/console_readline.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.Write("Nhap vao du lieu: ");
-        string str = Console.ReadLine();
-        Console.WriteLine("Du lieu vua nhap vao la: " + str);
-    }
-}
--- helloworld/helloworld.cs
+++
@@ -1,1 +0,0 @@
-﻿Console.Write("Hello, Toi la Dat");
