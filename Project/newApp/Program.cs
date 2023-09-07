c8e9797e4924cb81f5d4589411aed3cad9dcdeab
DatLwu <datluu351@gmail.com>
2023-09-07 17:42:20 +0700

    tinh hieu va tich, thuong va so du

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,10 +1,19 @@
 ﻿internal class Program{
     private static void Main(String[] args){
-        const int a=10;
+        int a;
+        System.Console.Write("gia tri cua a la: ");
+        a=Convert.ToInt32(Console.ReadLine());
         int b;
         System.Console.Write("gia tri cua b la: ");
         b=Convert.ToInt32(Console.ReadLine());
-        int c=a+b;
+        int c=a-b;
+        int d=a*b;
+        int e=a/b;
+        int f=a%b;
         System.Console.WriteLine("gia tri cua c= " +c);
+        System.Console.WriteLine("gia tri cua d= " +d);
+        System.Console.WriteLine("gia tri cua e= " +e,"va phan du la: "+f);
+        System.Console.WriteLine("va phan du la: "+f);
+
     }
 }
