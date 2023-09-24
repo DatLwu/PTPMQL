2f78bf91f8344381d3a749d1029d71c61f230813
DatLwu <datluu351@gmail.com>
2023-09-24 17:45:32 +0700

    BTH employee.cs ke thua tu Person.cs

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,20 +1,6 @@
-﻿internal class Program{
-    private static void Main(String[] args){
-        int a;
-        System.Console.Write("gia tri cua a la: ");
-        a=Convert.ToInt32(Console.ReadLine());
-        int b;
-        System.Console.Write("gia tri cua b la: ");
-        b=Convert.ToInt32(Console.ReadLine());
-        if (a>b){
-            System.Console.Write("a lon hon b ");
-            
-        }else if(a<b){
-            System.Console.Write("a nho hon b");
-        }else{
-            System.Console.Write("a bang b ");
-        }
+﻿using newApp.models;
+Employee emp = new Employee();
+emp.Nhap();
+emp.Xuat();
 
 
-    }
-}
