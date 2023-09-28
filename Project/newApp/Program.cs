a82b4d37a71e974650ea7cff8b05048d0296591d
DatLwu <datluu351@gmail.com>
2023-09-28 17:08:37 +0700

    tao array

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,9 +1,19 @@
 ﻿using newApp.models;
 
-Fruit2 fr = new Fruit2();
-fr.NhapTT();
-fr.XuatTT();
+int n;
+System.Console.Write("n = ");
+n = Convert.ToInt16(Console.ReadLine());
+
+int[] intArr = new int[n];
 
 
+for (int i = 0; i < intArr.Length; i++)
+{
+    intArr[0] = 2;
+    intArr[i] = i*2;
+    
 
+}
+for (int i = 0; i < intArr.Length; i++){
+    System.Console.WriteLine("Mang[" + i + "] = " + intArr[i]);}
 
